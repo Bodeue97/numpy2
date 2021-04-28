64a1a26b48066dc389131cd428ebd51b8442e072
Bodeu97 <lukaszebo@gmail.com>
2021-04-28 17:23:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+# Zad 1
+# a=np.arange(3)
+# b=np.arange(3)
+
+# print(a*b)
+
+# Zad2
+# a=np.arange(9).reshape(3,3)
+# b=np.arange(16).reshape(4,4)
+# def findSmallestInRowCol(a):
+#     answer = "Najmniejsze wartości w poszczególnych rzędach macierzy to: \n"
+#     for x in a:
+#         answer+= str(np.amin(x)) + "\n"
+#     a = a.T
+#     answer+="Najmniejsze wartości w poszczególnych kolumnach macierzy to: \n"
+#     for y in a:
+#         answer+=str(np.amin(y)) + "\n"
+#     return answer
+#
+# print(findSmallestInRowCol(a))
+# print(findSmallestInRowCol(b))
+#
+
+# Zad3
+# a=np.arange(3)
+# b=np.arange(3)
+# print(a.dot(b))
+
+# Zad4
+
+# a=np.ones(3, dtype="int32")
+# b=np.linspace(0,np.pi,3)
+# print(a.dot(b))
+
+
+# Zad5
+
